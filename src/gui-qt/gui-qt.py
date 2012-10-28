a769021ad3aa72f8979627ab943c11f10e346feb
Ivan Marin <ispmarin@gmail.com>
2012-10-27 23:49:39 -0400

    first progress in connecting gui with computing

--- src/gui-qt/gui-qt.py
+++ src/gui-qt/gui-qt.py
@@ -14,7 +14,8 @@ from PySide import QtCore
 from PySide.QtGui import QApplication, QMainWindow, QTextEdit,\
                          QPushButton,  QMessageBox, QWidget, QVBoxLayout
                          
-
+import system
+import flow
 
 __version__ = '0.0.1'
  
@@ -31,6 +32,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.main_frame.setupUi(self)
         
         
+        ## TODO: check the input strings for whitespaces or put default values
+        
+        #self.workhorse = system.system_def(float(self.main_frame.dim_x.text()), 
+        #                                   float(self.main_frame.dim_y.text()), float(self.main_frame.spacing.text()), 
+        #                                   0.0, float(self.main_frame.k.text()), float(self.main_frame.n.text()))
+        
+        #self.workhorse.fixed_boundary_conditions(float(self.main_frame.head_up.text()), float(self.main_frame.head_down.text())) 
+        
+        #self.flow = flow.flow_calc(self.main_frame.max_iter, self.main_frame.limit_convergence, flow_system) 
        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
